79aeb71c8b6bae99932293df5d570389c379cc0c
Sugak Andrey <sugak.andr3y@gmail.com>
2018-02-22 02:06:33 +0300

    Implement stack machine compiler

--- src/SM.ml
+++ src/SM.ml
@@ -1,20 +1,23 @@
-open GT       
-       
+open GT
+open Syntax
+
+module L = List
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
    interpreter
- *)
+*)
 type config = int list * Syntax.Stmt.config
 
 (* Stack machine interpreter
@@ -22,8 +25,20 @@ type config = int list * Syntax.Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+*)
+let eval' (stack, state, is, os) = function
+  | CONST v  -> (v :: stack, state, is, os)
+  | READ     -> (L.hd is :: stack, state, L.tl is, os)
+  | WRITE    -> (L.tl stack, state, is, L.hd stack :: os)
+  | LD name  -> (state name :: stack, state, is, os)
+  | ST name  -> (L.tl stack, Expr.update name (L.hd stack) state, is, os)
+  | BINOP op ->
+    let f = Expr.getBinOpFun op in
+    let y :: x :: rem = stack in
+    (f x y :: rem, state, is, os)
+
+
+let eval = L.fold_left eval'
 
 (* Stack machine compiler
 
@@ -31,6 +46,16 @@ let eval _ = failwith "Not yet implemented"
 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
- *)
+*)
 
-let compile _ = failwith "Not yet implemented"
+let rec compile' = function
+| Expr.Const v         -> [CONST v]
+| Expr.Var name        -> [LD name]
+| Expr.Binop (f, x, y) -> compile' x @ compile' y @ [BINOP f]
+
+let rec compile = function
+  | Stmt.Read s           -> [READ; ST s]
+  | Stmt.Write e          -> compile' e @ [WRITE]
+  | Stmt.Assign (name, e) -> compile' e @ [ST name]
+  | Stmt.Seq (fst, snd)   -> compile fst @ compile snd
+
--- src/Syntax.ml
+++ src/Syntax.ml
@@ -1,14 +1,14 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-    
+open GT
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -22,29 +22,57 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
-      to value v and returns the new state.
+    (* Update: non-destructively "modifies" the state s by binding the variable x
+       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
 
     (* Expression evaluator
 
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let ($$) f g x y = f (g x) (g y)
 
-  end
-                    
+    let getBinOpFun op =
+      let toBool     = (<>) 0 in
+      let fromBool v = if v then 1 else 0
+      in match op with
+      | "+" -> (+)
+      | "-" -> (-)
+      | "*" -> ( * )
+      | "/" -> (/)
+      | "%" -> (mod)
+      | _   ->
+        let boolFun = match op with
+          | ">"  -> (>)
+          | "<"  -> (<)
+          | ">=" -> (>=)
+          | "<=" -> (<=)
+          | "==" -> (=)
+          | "!=" -> (<>)
+          | "!!" -> (||) $$ toBool
+          | "&&" -> (&&) $$ toBool
+          | _    -> failwith "Unsupported binary operator."
+        in fun x y -> fromBool @@ boolFun x y
+
+    let rec eval state = function
+      | Const v         -> v
+      | Var name        -> state name
+      | Binop(op, x, y) ->
+        let f = (getBinOpFun op) $$ (eval state)
+        in f x y
+end
+
 (* Simple statements: syntax and sematics *)
 module Stmt =
   struct
@@ -57,7 +85,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -65,6 +93,12 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
-                                                         
-  end
+    module L = List
+    module E = Expr
+
+    let rec eval (state, is, os) = function
+      | Read name        -> (E.update name (L.hd is) state, L.tl is, os)
+      | Write e          -> (state, is, (E.eval state e) :: os)
+      | Assign (name, e) -> (E.update name (E.eval state e) state, is, os)
+      | Seq (fst, snd)   -> eval (eval (state, is, os) fst) snd
+end
