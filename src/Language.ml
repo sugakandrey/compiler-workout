f92ac0491d176d49b4daab5c56896b0fdcbf986c
Sugak Andrey <sugak.andr3y@gmail.com>
2018-03-07 20:37:25 +0300

    Implement expression parsers

--- src/Language.ml
+++ src/Language.ml
@@ -5,13 +5,13 @@ open GT
 
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
-       
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -25,39 +25,83 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
-      to value v and returns the new state.
+    (* Update: non-destructively "modifies" the state s by binding the variable x
+       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
 
     (* Expression evaluator
 
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let ($$) f g x y = f (g x) (g y)
+
+    let getBinOpFun op =
+      let toBool     = (<>) 0 in
+      let fromBool v = if v then 1 else 0
+      in match op with
+      | "+" -> (+)
+      | "-" -> (-)
+      | "*" -> ( * )
+      | "/" -> (/)
+      | "%" -> (mod)
+      | _   ->
+        let boolFun = match op with
+          | ">"  -> (>)
+          | "<"  -> (<)
+          | ">=" -> (>=)
+          | "<=" -> (<=)
+          | "==" -> (=)
+          | "!=" -> (<>)
+          | "!!" -> (||) $$ toBool
+          | "&&" -> (&&) $$ toBool
+          | _    -> failwith "Unsupported binary operator."
+        in fun x y -> fromBool @@ boolFun x y
+
+    let rec eval state = function
+      | Const v         -> v
+      | Var name        -> state name
+      | Binop(op, x, y) ->
+        let f = (getBinOpFun op) $$ (eval state)
+        in f x y
 
     (* Expression parser. You can use the following terminals:
 
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
          DECIMAL --- a decimal constant [0-9]+ as a string
-   
-    *)
-    ostap (
-      parse: empty {failwith "Not implemented yet"}
-    )
 
-  end
-                    
+    *)
+    let operators l = List.map (fun s -> (ostap ($(s)), fun x y -> Binop(s, x, y))) l
+
+    ostap (
+      primary: x:IDENT {Var x}
+             | x:DECIMAL {Const x}
+             | -"(" parse -")";
+
+      parse: !(Ostap.Util.expr
+                 (fun x -> x)
+                 [|
+                   `Lefta, operators ["!!"];
+                   `Lefta, operators ["&&"];
+                   `Nona,  operators [">="; ">"; "<="; "<"; "=="; "!="];
+                   `Lefta, operators ["+"; "-"];
+                   `Lefta, operators ["*"; "/"; "%"]
+                 |]
+                 primary
+              )
+    )
+end
+
 (* Simple statements: syntax and sematics *)
 module Stmt =
   struct
@@ -70,7 +114,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -78,19 +122,34 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    module L = List
+    module E = Expr
+
+    let rec eval (state, is, os) = function
+      | Read name        -> (E.update name (L.hd is) state, L.tl is, os)
+      | Write e          -> (state, is, (E.eval state e) :: os)
+      | Assign (name, e) -> (E.update name (E.eval state e) state, is, os)
+      | Seq (fst, snd)   -> eval (eval (state, is, os) fst) snd
 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      primary: -"read"  -"(" v:IDENT -")" { Read v }
+             | -"write" -"(" e:!(Expr.parse) -")" { Write e }
+             | v:IDENT -":=" e:!(Expr.parse) { Assign (v, e) };
+               parse: !(Ostap.Util.expr
+                          (fun x -> x)
+                          [|
+                            `Righta, [ostap (";"), fun x y -> Seq(x, y)]
+                          |]
+                          primary
+                       )
     )
-      
-  end
+end
 
 (* The top-level definitions *)
 
 (* The top-level syntax category is statement *)
-type t = Stmt.t    
+type t = Stmt.t
 
 (* Top-level evaluator
 
--- src/SM.ml
+++ src/SM.ml
@@ -1,21 +1,22 @@
-open GT       
+open GT
 open Language
-       
+module L = List
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
    interpreter
- *)
+*)
 type config = int list * Stmt.config
 
 (* Stack machine interpreter
@@ -23,8 +24,19 @@ type config = int list * Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+*)
+let eval =
+  let eval' (stack, (state, is, os)) = function
+    | CONST v  -> (v :: stack, (state, is, os))
+    | READ     -> (L.hd is :: stack, (state, L.tl is, os))
+    | WRITE    -> (L.tl stack, (state, is, L.hd stack :: os))
+    | LD name  -> (state name :: stack, (state, is, os))
+    | ST name  -> (L.tl stack, (Expr.update name (L.hd stack) state, is, os))
+    | BINOP op ->
+      let f = Expr.getBinOpFun op in
+      let y :: x :: rem = stack in
+      (f x y :: rem, (state, is, os))
+  in L.fold_left eval'
 
 (* Top-level evaluation
 
@@ -40,5 +52,14 @@ let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in 
 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
- *)
-let compile _ = failwith "Not yet implemented"
+*)
+let rec compile' = function
+  | Expr.Const v         -> [CONST v]
+  | Expr.Var name        -> [LD name]
+  | Expr.Binop (f, x, y) -> compile' x @ compile' y @ [BINOP f]
+
+let rec compile = function
+  | Stmt.Read s           -> [READ; ST s]
+  | Stmt.Write e          -> compile' e @ [WRITE]
+  | Stmt.Assign (name, e) -> compile' e @ [ST name]
+  | Stmt.Seq (fst, snd)   -> compile fst @ compile snd
